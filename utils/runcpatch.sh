f3ae82a4a6d3ee1e44855f5abd7b3b2f52b5a944
xleng <34652413+lengxiaohua123@users.noreply.github.com>
2024-10-20 16:52:26 +0800

    更新 runcpatch.sh

--- utils/runcpatch.sh
+++ utils/runcpatch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 RUNC_ADD_FILE=$1
 row=$(sed -n -e '/^static int cgroup_add_file/=' $RUNC_ADD_FILE)
+echo "" > test2.txt
 touch test2.txt
 sed -n -e '/static int cgroup_add_file/,/return 0/p' $RUNC_ADD_FILE >> test2.txt
 row2=$(sed -n -e '/return 0/=' test2.txt)
