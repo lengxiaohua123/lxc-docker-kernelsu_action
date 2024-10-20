0ab6967c8a58067ca86d7ea68241ecf79cb67512
xleng <34652413+lengxiaohua123@users.noreply.github.com>
2024-10-20 16:36:08 +0800

    更新 runcpatch.sh

--- utils/runcpatch.sh
+++ utils/runcpatch.sh
@@ -9,3 +9,4 @@ sed -i "$row3 i\        }" $RUNC_ADD_FILE
 sed -i "$row3 i\                kernfs_create_link(cgrp->kn, name, kn);" $RUNC_ADD_FILE
 sed -i "$row3 i\                snprintf(name, CGROUP_FILE_NAME_MAX, \"%s.%s\", cft->ss->name, cft->name);" $RUNC_ADD_FILE
 sed -i "$row3 i\        if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {" $RUNC_ADD_FILE
+rm test2.txt
